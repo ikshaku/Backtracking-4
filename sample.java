10631418dbaad9db811fdba0ece4b39c2e4869aa
Ikshaku Goswami <ig752994@gmail.com>
2022-01-11 10:38:56 +0530

    completed

--- sample.java
+++ sample.java
@@ -1,7 +1,74 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
-
-// Your code here along with comments explaining your approach
+//brace expansion
+//tc exponential
+//sc size of string
+class Solution {
+    public String[] expand(String s) {
+        
+        List<List<Character>> blocks = new ArrayList<>();
+        int i = 0;
+        
+        while(i<s.length()){
+            
+            List<Character> block = new ArrayList<>();
+            
+            if(s.charAt(i)=='{'){
+                
+                i++;
+                
+                while(s.charAt(i)!='}'){
+                    
+                    if(s.charAt(i)!=','){
+                        
+                        block.add(s.charAt(i));
+                    
+                    }
+                    
+                    i++;
+                }
+                
+            }
+            else{
+                
+                block.add(s.charAt(i));
+                
+            }
+            blocks.add(block);
+            i++;
+        }
+        
+        for(int j = 0; j < blocks.size(); j++){
+            System.out.println(blocks.get(j));
+        }
+        
+        List<String> result = new ArrayList<>();
+        
+        helper(blocks, 0, new StringBuilder(), result);
+        
+        Collections.sort(result);
+        
+        return result.toArray(new String[0]);
+    }
+    
+    private void helper(List<List<Character>> blocks, int i, StringBuilder curr, List<String> result){
+        
+        if(i==blocks.size()){
+            
+            result.add(curr.toString());
+            return;
+            
+        }
+        
+        for(int j = 0; j < blocks.get(i).size(); j++){
+            
+            
+            curr.append(blocks.get(i).get(j));
+            
+            helper(blocks, i+1, curr, result);
+            
+            curr.deleteCharAt(curr.length() - 1);
+            
+        }
+        return;
+    }
+    
+}
